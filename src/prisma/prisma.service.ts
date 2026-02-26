503672d5438b73564f5c37897accce9d5ff497ba
narpalan <thiagofumega@gmail.com>
2026-02-26 20:29:39 -0300

    :recycle: refactor: implementa adaptador para conexão ao banco de dados.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,24 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {  
+  constructor(private configService: ConfigService) {
+    const adapter = new PrismaMariaDb({
+      host: configService.get<string>('DB_HOST'),
+      user: configService.get<string>('DB_USER'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_DATABASE'),
+      port: configService.get<number>('DB_PORT'), 
+      allowPublicKeyRetrieval: true,
+      ssl: false,     
+    });    
+
+    super({adapter})
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
