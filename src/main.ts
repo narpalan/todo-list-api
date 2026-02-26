5f3720931349dca403462714eb10e678b26ba96a
narpalan <thiagofumega@gmail.com>
2026-02-26 20:28:53 -0300

    :recycle: refactor: altera endpoint da documentação do swagger

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
       .addBearerAuth()
       .build();
       const document = SwaggerModule.createDocument(app, config);
-      SwaggerModule.setup('api', app, document);
+      SwaggerModule.setup('docs', app, document);
   }
 
   const port = configService.get<string>('PORT');
