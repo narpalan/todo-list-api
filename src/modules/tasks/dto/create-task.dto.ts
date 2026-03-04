33e67de62fe10efc2c1f55df56f5b41b4076a048
narpalan <thiagofumega@gmail.com>
2026-03-04 13:06:23 -0300

    feat: integra validador no DTO de criação da tarefa

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsDateString, IsNotEmpty } from 'class-validator';
+import { IsString, IsDateString, IsNotEmpty, Validate } from 'class-validator';
+import { IsFutureDateConstraint } from './is-future-data.validator';
 
 export class CreateTaskDto {
   @ApiProperty({ example: 'Estudar NestJS' })
@@ -9,5 +10,6 @@ export class CreateTaskDto {
 
   @ApiProperty({ example: '2025-12-31T23:59:59Z' })
   @IsDateString()
+  @Validate(IsFutureDateConstraint)
   dueDate: string;
 }
